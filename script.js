306eeccf43402c146f9b1ec5d0af35e23a449e97
shotadz3 <shota.shotadze.1@btu.edu.ge>
2025-07-06 23:30:56 +0400

    changes in js

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     setupEventListeners();
     showCookieNotification();
     fetchQuote();
+    showFavorites();
 });
 
 function setupEventListeners() {
